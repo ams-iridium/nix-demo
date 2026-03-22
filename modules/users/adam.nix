e1ae536c706c2eed10e55859063d7486999cef78
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-22 01:05:41 -0400

    add vscodeserver

--- modules/users/adam.nix
+++ modules/users/adam.nix
@@ -8,7 +8,8 @@
       "wheel"
     ];
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJMjtOqSWLDq79t/9XljmBrfBVm8deQJdOQmTV7c45Ni adam" # content of authorized_keys file
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJMjtOqSWLDq79t/9XljmBrfBVm8deQJdOQmTV7c45Ni adam@malak"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIojZ/xu4CVq5TbY51CMUlRiWnSdkS7ZN9xL10gNrFux black@plagueis"
     ];
   };
 }
