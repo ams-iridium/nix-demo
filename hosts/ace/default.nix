aae9c6179213bf2fedb5edcfa6c63a265164f918
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-21 23:42:18 -0400

    rework user account

--- hosts/ace/default.nix
+++ hosts/ace/default.nix
@@ -14,6 +14,23 @@ inputs.nixos-raspberrypi.lib.nixosSystem
         raspberry-pi-5.bluetooth
       ];
       boot.loader.raspberry-pi.bootloader = "kernel";
+      # This is the initial version of nixOS that was installed on this system.
+      system.stateVersion = "25.11";
+      nix = {
+        settings = {
+          experimental-features = [ "nix-command" "flakes" ];
+        };
+      };
+      networking.hostName = "ace";
+      networking.firewall.enable = true;
+      networking.firewall.allowedTCPPorts = [ 80 443 ];
+      security.sudo.wheelNeedsPassword = false;
+      services.openssh.enable = true;
+      services.avahi = {
+        enable = true;
+        nssmdns4 = true;
+      };
+      services.pcscd.enable = true;
     })
 
     ({ ... }: {
--- modules/users/adam.nix
+++ modules/users/adam.nix
@@ -1,17 +1,9 @@
 { ... }: 
 {
-  # This is the initial version of nixOS that was installed on this system.
-  system.stateVersion = "25.11";
-  nix = {
-    settings = {
-      experimental-features = [ "nix-command" "flakes" ];
-    };
-  };
-  networking.hostName = "ace";
-  networking.firewall.enable = true;
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
   users.users.adam = {
     isNormalUser = true;
+    description = "Test account for Adam Schafer";
+    # Sudo access
     extraGroups = [
       "wheel"
     ];
@@ -19,11 +11,4 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJMjtOqSWLDq79t/9XljmBrfBVm8deQJdOQmTV7c45Ni adam" # content of authorized_keys file
     ];
   };
-  security.sudo.wheelNeedsPassword = false;
-  services.openssh.enable = true;
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-  };
-  services.pcscd.enable = true;
 }
