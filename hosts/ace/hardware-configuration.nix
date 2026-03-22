59e0a5041f6c485015c5cfc27d4f632f166d72cc
ams-tech <ams-tech@users.noreply.github.com>
2026-03-22 08:26:46 +0000

    add data filesystem

--- hosts/ace/hardware-configuration.nix
+++ hosts/ace/hardware-configuration.nix
@@ -1,7 +1,8 @@
 { nixos-raspberrypi, ... }: {
   imports = with nixos-raspberrypi.nixosModules; [
     raspberry-pi-5.base
-    raspberry-pi-5.bluetooth
+    raspberry-pi-5.page-size-16k
+    raspberry-pi-5.display-vc4
   ];
   fileSystems = {
     "/boot/firmware" = {
@@ -18,6 +19,15 @@
       device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
       fsType = "ext4";
       options = [ "noatime" ];
+    };
+    "/data" = {
+      device = "/dev/disk/by-label/NixOS-Data";
+      options = [ # If you don't have this options attribute, it'll default to "defaults" 
+        # boot options for fstab. Search up fstab mount options you can use
+        "users" # Allows any user to mount and unmount
+        "nofail" # Prevent system from failing if this drive doesn't mount
+        "exec" # Permit execution of binaries and other executable files
+      ];
     };
   };
   boot.loader.raspberry-pi.bootloader = "kernel";
