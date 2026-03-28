a491371665a73b86664ae4b54636e8310e16c2bb
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-28 05:17:48 -0400

    add service to generate salt

--- modules/rpi-otp-luks-key.nix
+++ modules/rpi-otp-luks-key.nix
@@ -1,7 +1,8 @@
-{  ... }: 
+{ pkgs,  ... }: 
 let
   secretsDirectory = "/run/secrets";
   luksKeyFile = "${secretsDirectory}/luks.key";
+  luksKeySalt = "saltysaltsalt";
 
   getKeyService = extraConfig: {
     description = "Get the luks key from Raspberry Pi OTP.";
@@ -12,7 +13,7 @@ let
     script = ''
       install -d -m 0700 ${secretsDirectory}
       # replace this with rpi-otp-private-key logic
-      echo 12345 > ${luksKeyFile}
+      echo "$(${pkgs.raspberrypi-eeprom}/bin/rpi-otp-private-key)${luksKeySalt}" | sha256sum | tr -d ' -' > '${luksKeyFile}' 
       chmod 0400 ${luksKeyFile}
     '';
   } // extraConfig;
@@ -21,9 +22,5 @@ in
   boot.initrd.systemd.enable = true;
   systemd.services.rpi-otp-luks-key = getKeyService {
     wantedBy = [ "multi-user.target" ];
-  };
-  boot.initrd.systemd.services.rpi-otp-luks-key = getKeyService {
-    wantedBy = [ "initrd.target" ];
-    unitConfig.DefaultDependencies = false;
   };
 }
