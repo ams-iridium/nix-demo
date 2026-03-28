5df8d87b62da2ef2f620389b993d758a9652ca7f
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-28 02:31:49 -0400

    tweak luks key isntaller

--- modules/rpi-otp-luks-key.nix
+++ modules/rpi-otp-luks-key.nix
@@ -1,7 +1,9 @@
 {  ... }: 
+let
+  secretsDirectory = "/run/secrcrets";
+  luksKeyFile = "${secretsDirectory}/luks.key";
+in
 {
-  boot.initrd.systemd.enable = true;
-
   systemd.services.rpi-otp-luks-key = {
     description = "Create temporary initrd secret";
     wantedBy = [ "multi-user.target" ];
@@ -9,10 +11,10 @@
       Type = "oneshot";
     };
     script = ''
-      install -d -m 0700 /run/secrets
+      install -d -m 0700 ${secretsDirectory}
       # replace this with rpi-otp-private-key logic
-      echo 12345 > /run/secrets/luks.key
-      chmod 0400 /run/secrets/luks.key
+      echo 12345 > ${luksKeyFile}
+      chmod 0400 ${luksKeyFile}
     '';
   };
 }
