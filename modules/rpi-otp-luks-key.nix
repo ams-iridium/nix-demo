5a1ff2431e025de277f1a7c1550db8d9bc77b341
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-28 07:18:26 -0400

    exit on error

--- modules/rpi-otp-luks-key.nix
+++ modules/rpi-otp-luks-key.nix
@@ -8,8 +8,8 @@ let
     # Exit on any error
     set -e
     # The '-c' flag ensures the key is not all 0s.
-    otp_secret=$(${pkgs.raspberrypi-eeprom}/bin/rpi-otp-private-key -c)
-    echo "${luksKeySalt}$otp_secret" | cut -c1-25
+    SECRET=$(${pkgs.raspberrypi-eeprom}/bin/rpi-otp-private-key)
+    echo "${luksKeySalt}$SECRET" | cut -c1-25
   '';
 
   getKeyService = extraConfig: {
