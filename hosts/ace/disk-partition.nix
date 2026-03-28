16628835d862b29d1ad7810c97a88e9ee07365ed
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-27 20:14:03 -0400

    remove luks partition

--- hosts/ace/disk-partition.nix
+++ hosts/ace/disk-partition.nix
@@ -92,37 +92,6 @@
                 mountpoint = "/";
               };
             };
-            luks = {
-              size = "100%";
-              content = {
-                type = "luks";
-                name = "crypted";
-                extraOpenArgs = [ ];
-                settings = {
-                  keyFile = "/run/luks.key";
-                  allowDiscards = true;
-                };
-                content = {
-                  type = "lvm_pv";
-                  vg = "pool";
-                };
-              };
-            };
-          };
-        };
-      };
-    };
-    lvm_vg = {
-      pool = {
-        type = "lvm_vg";
-        lvs = {
-          data = {
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/data";
-            };
           };
         };
       };
