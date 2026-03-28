7aa12417e9aa569192188fbb3fa0eb7a2a9bcbb4
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-28 17:37:14 -0400

    use initrdbin

--- modules/rpi-otp-luks-key.nix
+++ modules/rpi-otp-luks-key.nix
@@ -6,11 +6,16 @@ let
 
   keygenScript = pkgs.writeShellScriptBin "rpi-gen-luks-key" ''
     # Exit on any error
-    set -e
+    # set -e
     # The '-c' flag ensures the key is not all 0s.
-    ${pkgs.raspberrypi-eeprom}/bin/rpi-otp-private-key -c 
-    RPI_OTP_SECRET=$(${pkgs.raspberrypi-eeprom}/bin/rpi-otp-private-key)
-    echo "${luksKeySalt}$RPI_OTP_SECRET" | sha256sum | tr -d ' -'
+    ${pkgs.raspberrypi-eeprom}/bin/rpi-otp-private-key --help
+    if [ $? -ne 1 ]; then
+      echo "failed with unexpected code"
+      exit 5
+    fi
+    # ${pkgs.raspberrypi-eeprom}/bin/rpi-otp-private-key -c 
+    # RPI_OTP_SECRET=$(${pkgs.raspberrypi-eeprom}/bin/rpi-otp-private-key)
+    # echo "${luksKeySalt}$RPI_OTP_SECRET" | sha256sum | tr -d ' -'
   '';
 
   getKeyService = extraConfig: {
@@ -44,7 +49,7 @@ in
   boot.initrd.systemd.extraBin = {
     rpi-gen-luks-key = "${keygenScript}/bin/rpi-gen-luks-key";
     rpi-otp-private-key = "${pkgs.raspberrypi-eeprom}/bin/rpi-otp-private-key";
-    
+
   };
 
   environment.systemPackages = [
