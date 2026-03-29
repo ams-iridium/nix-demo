37395e24598b85226750655afdf5f6eacc37d36a
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-29 18:13:17 -0400

    add encrypted disk partition

--- hosts/ace/disk-partition.nix
+++ hosts/ace/disk-partition.nix
@@ -68,6 +68,43 @@
                 mountpoint = "/";
               };
             };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                extraOpenArgs = [ ];
+                settings = {
+                  keyFile = "/run/luks.key";
+                  allowDiscards = true;
+                  preOpenCommands = ''
+                    echo "12345" > "/run/luks.key"
+                  '';
+                  postOpenCommands = ''
+                    rm "/run/luks.key"
+                  '';
+                };
+                content = {
+                  type = "lvm_pv";
+                  vg = "pool";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          data = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/data";
+            };
           };
         };
       };
