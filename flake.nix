3a5a691ba1397b9c38f8cb1ec12bb49aae47ce5e
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-29 16:55:14 -0400

    remove unneeded system packages

--- flake.nix
+++ flake.nix
@@ -65,8 +65,6 @@
             networking.nameservers = [ "8.8.8.8" "8.8.4.4" "2001:4860:4860::8888" "2001:4860:4860::8844"];
             environment.systemPackages = [
               installScript
-              pkgs.rpi-otp-private-key
-              pkgs.rpi-otp-luks-key
             ];
           })
         ];
