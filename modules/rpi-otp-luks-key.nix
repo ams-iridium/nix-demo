7c9afd84c84e3f47b6323a5d78c3a8c36882fe9e
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-29 14:35:32 -0400

    enable initrd service

--- modules/rpi-otp-luks-key.nix
+++ modules/rpi-otp-luks-key.nix
@@ -24,18 +24,18 @@ in
   };
 
 
-#  boot.initrd.systemd.services.rpi-otp-luks-key-initrd = getKeyService {
-#    wantedBy = [ "initrd.target" ];
-#    before = [
-#      "initrd-root-device.target"   # before disk discovery/mount
-#      "sysroot.mount"
-#    ];
-#    unitConfig.DefaultDependencies = false;
-#  };
-#  boot.initrd.systemd.extraBin = {
-#    rpi-gen-luks-key = "${keygenScript}/bin/rpi-gen-luks-key";
-#    rpi-otp-private-key = "${pkgs.rpi-otp-private-key}/bin/rpi-otp-private-key";
-#    vcgencmd = "${pkgs.libraspberrypi}/bin/vcgencmd";
-#    vcmailbox = "${pkgs.libraspberrypi}/bin/vcmailbox";
-#  };
+  boot.initrd.systemd.services.rpi-otp-luks-key-initrd = getKeyService {
+    wantedBy = [ "initrd.target" ];
+    before = [
+      "initrd-root-device.target"   # before disk discovery/mount
+      "sysroot.mount"
+    ];
+    unitConfig.DefaultDependencies = false;
+  };
+  boot.initrd.systemd.extraBin = {
+    rpi-gen-luks-key = "${pkgs.rpi-otp-luks-key}/bin/rpi-gen-luks-key";
+    # rpi-otp-private-key = "${pkgs.rpi-otp-private-key}/bin/rpi-otp-private-key";
+    # vcgencmd = "${pkgs.libraspberrypi}/bin/vcgencmd";
+    # vcmailbox = "${pkgs.libraspberrypi}/bin/vcmailbox";
+  };
 }
