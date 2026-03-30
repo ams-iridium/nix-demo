0b546e21147004a8292c57af111fd95176dec807
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-30 14:34:11 -0400

    add provision command to installer

--- flake.nix
+++ flake.nix
@@ -74,7 +74,7 @@
             };
             services.getty.autologinUser = "adam";
             environment.systemPackages = [
-              installScript
+              pkgs.rpi-otp-provision-private-key
             ];
             nixpkgs.overlays = [ self.overlays.default ];
             networking.nameservers = [ "8.8.8.8" "8.8.4.4" "2001:4860:4860::8888" "2001:4860:4860::8844"];
