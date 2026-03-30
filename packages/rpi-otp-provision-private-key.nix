b92cee630142cafcc89131ce19fdf202d0353295
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-30 00:32:14 -0400

    fix private key provision flake

--- packages/rpi-otp-provision-private-key.nix
+++ packages/rpi-otp-provision-private-key.nix
@@ -14,12 +14,6 @@ writeShellScriptBin "rpi-otp-provision-private-key" ''
   
   OPTIONAL_SALT="$1"
   # The '-c' flag ensures the key is not all 0s.
-  rpi-otp-private-key -c
-  CHECK_KEY_RESULT="$?"
-  if [ "$CHECK_KEY_RESULT" -eq "0" ]; then
-    echo "Secure key has already been written (or couldn't be read)"
-    exit 1
-  fi
   RANDOM_KEY=$(openssl rand -hex 32)
   if [ "''${#RANDOM_KEY}" -ne "32" ]; then
     echo "Failed to generate random key"
